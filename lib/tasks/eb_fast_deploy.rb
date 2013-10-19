f90720d8a54f1c5cbf87db2f937425d8d90d7308
Nicola Brisotto <nicolabrisotto@gmail.com>
2013-10-19 16:41:58 +0200

    Add support for RDS and Network options file

--- lib/tasks/eb_fast_deploy.rb
+++ lib/tasks/eb_fast_deploy.rb
@@ -3,6 +3,80 @@ require 'aws-sdk'
 require 'dotenv/environment'
 require 'fog'
 
+module EbFastDeploy
+
+  OPTIONS = {
+    "AvailabilityZones" => {:namespace => "aws:autoscaling:asg", :option_name => "Availability Zones"},
+    "Cooldown" => {:namespace => "aws:autoscaling:asg", :option_name => "Cooldown"},
+    "CustomAvailabilityZones" => {:namespace => "aws:autoscaling:asg", :option_name => "Custom Availability Zones"},
+    "MinSize" => {:namespace => "aws:autoscaling:asg", :option_name => "MinSize"},
+    "MaxSize" => {:namespace => "aws:autoscaling:asg", :option_name => "MaxSize"},
+
+    "EC2KeyName" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "EC2KeyName"},
+    "IamInstanceProfile" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "IamInstanceProfile"},
+    "ImageId" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "ImageId"},
+    "InstanceType" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "InstanceType"},
+    "MonitoringInterval" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "MonitoringInterval"},
+    "SecurityGroups" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "SecurityGroups"},
+    "SSHSourceRestriction" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "SSHSourceRestriction"},
+    "BlockDeviceMappings" => {:namespace => "aws:autoscaling:launchconfiguration", :option_name => "BlockDeviceMappings"},
+    "LogPublicationControl" => {:namespace => "aws:elasticbeanstalk:hostmanager", :option_name => "LogPublicationControl"},
+
+    "BreachDuration" => {:namespace => "aws:autoscaling:trigger", :option_name => "BreachDuration"},
+    "LowerBreachScaleIncrement" => {:namespace => "aws:autoscaling:trigger", :option_name => "LowerBreachScaleIncrement"},
+    "LowerThreshold" => {:namespace => "aws:autoscaling:trigger", :option_name => "LowerThreshold"},
+    "MeasureName" => {:namespace => "aws:autoscaling:trigger", :option_name => "MeasureName"},
+    "Period" => {:namespace => "aws:autoscaling:trigger", :option_name => "Period"},
+    "Statistic" => {:namespace => "aws:autoscaling:trigger", :option_name => "Statistic"},
+    "Unit" => {:namespace => "aws:autoscaling:trigger", :option_name => "Unit"},
+    "UpperBreachScaleIncrement" => {:namespace => "aws:autoscaling:trigger", :option_name => "UpperBreachScaleIncrement"},
+    "UpperThreshold" => {:namespace => "aws:autoscaling:trigger", :option_name => "UpperThreshold"},
+
+    "VPCId" => {:namespace => "aws:ec2:vpc", :option_name => "VPCId"},
+    "Subnets" => {:namespace => "aws:ec2:vpc", :option_name => "Subnets"},
+    "ELBSubnets" => {:namespace => "aws:ec2:vpc", :option_name => "ELBSubnets"},
+    "ELBScheme" => {:namespace => "aws:ec2:vpc", :option_name => "ELBScheme"},
+    "DBSubnets" => {:namespace => "aws:ec2:vpc", :option_name => "DBSubnets"},
+
+    "ApplicationHealthcheckURL" => {:namespace => "aws:elasticbeanstalk:application", :option_name => "Application Healthcheck URL"},
+
+    "EnvironmentType" => {:namespace => "aws:elasticbeanstalk:environment", :option_name => "EnvironmentType"},
+
+    "AutomaticallyTerminateUnhealthyInstances" => {:namespace => "aws:elasticbeanstalk:monitoring", :option_name => "Automatically Terminate Unhealthy Instances"},
+
+    "NotificationEndpoint" => {:namespace => "aws:elasticbeanstalk:sns:topics", :option_name => "Notification Endpoint"},
+    "NotificationProtocol" => {:namespace => "aws:elasticbeanstalk:sns:topics", :option_name => "Notification Protocol"},
+    "NotificationTopicARN" => {:namespace => "aws:elasticbeanstalk:sns:topics", :option_name => "Notification Topic ARN"},
+    "NotificationTopicName" => {:namespace => "aws:elasticbeanstalk:sns:topics", :option_name => "Notification Topic Name"},
+
+    "HealthyThreshold" => {:namespace => "aws:elb:healthcheck", :option_name => "HealthyThreshold"},
+    "Interval" => {:namespace => "aws:elb:healthcheck", :option_name => "Interval"},
+    "Timeout" => {:namespace => "aws:elb:healthcheck", :option_name => "Timeout"},
+    "UnhealthyThreshold" => {:namespace => "aws:elb:healthcheck", :option_name => "UnhealthyThreshold"},
+
+    "LoadBalancerHTTPPort" => {:namespace => "aws:elb:loadbalancer", :option_name => "LoadBalancerHTTPPort"},
+    "LoadBalancerPortProtocol" => {:namespace => "aws:elb:loadbalancer", :option_name => "LoadBalancerPortProtocol"},
+    "LoadBalancerHTTPSPort" => {:namespace => "aws:elb:loadbalancer", :option_name => "LoadBalancerHTTPSPort"},
+    "LoadBalancerSSLPortProtocol" => {:namespace => "aws:elb:loadbalancer", :option_name => "LoadBalancerSSLPortProtocol"},
+    "SSLCertificateId" => {:namespace => "aws:elb:loadbalancer", :option_name => "SSLCertificateId"},
+
+
+    "StickinessCookieExpiration" => {:namespace => "aws:elb:policies", :option_name => "Stickiness Cookie Expiration"},
+    "StickinessPolicy" => {:namespace => "aws:elb:policies", :option_name => "Stickiness Policy"},
+
+    "DBAllocatedStorage" => {:namespace => "aws:rds:dbinstance", :option_name => "DBAllocatedStorage"},
+    "DBDeletionPolicy" => {:namespace => "aws:rds:dbinstance", :option_name => "DBDeletionPolicy"},
+    "DBEngine" => {:namespace => "aws:rds:dbinstance", :option_name => "DBEngine"},
+    "DBEngineVersion" => {:namespace => "aws:rds:dbinstance", :option_name => "DBEngineVersion"},
+    "DBInstanceClass" => {:namespace => "aws:rds:dbinstance", :option_name => "DBInstanceClass"},
+    "DBPassword" => {:namespace => "aws:rds:dbinstance", :option_name => "DBPassword"},
+    "DBSnapshotIdentifier" => {:namespace => "aws:rds:dbinstance", :option_name => "DBSnapshotIdentifier"},
+    "DBUser" => {:namespace => "aws:rds:dbinstance", :option_name => "DBUser"},
+    "MultiAZDatabase" => {:namespace => "aws:rds:dbinstance", :option_name => "MultiAZDatabase"},
+  }.freeze
+
+end
+
 def do_cmd(cmd)
   print "- - - cmd: #{cmd}\n"
   result = `#{cmd}`
@@ -33,6 +107,15 @@ def set_vars
   @env_file_path = Rails.root.to_s+"/config/eb_environments/#{ENV['ENVIRONMENT']}/ruby_container_options"
   if File.exists?(@env_file_path)
     @eb_ruby_container_options = Dotenv::Environment.new(@env_file_path)
+  else
+    puts "(Warning) ruby_container_options doesn't exists"
+  end
+
+  @rdb_network_options_path = Rails.root.to_s+"/config/eb_environments/#{ENV['ENVIRONMENT']}/rdb_network_options"
+  if File.exists?(@env_file_path)
+    @rdb_network_options = Dotenv::Environment.new(@rdb_network_options_path);
+  else
+    puts "(Warning) rdb_network_options doesn't exists"
   end
 
   AWS.config(
@@ -50,10 +133,44 @@ def rails_options
   opts
 end
 
+def rdb_network_options
+  opts = []
+  @rdb_network_options.each do |k,v|
+    raise "(Error) Unknown option \"#{k}\"=#{v} in #{@rdb_network_options_path}, please check Elastic Beanstalk documentation and EbFastDeploy::OPTIONS" unless EbFastDeploy::OPTIONS.include?(k)
+    opts << {:namespace => EbFastDeploy::OPTIONS[k][:namespace], :option_name =>EbFastDeploy::OPTIONS[k][:option_name], :value=>v}
+  end
+  opts
+end
+
+def all_options
+  rdb_network_options + rails_options
+end
+
+
 namespace :eb do
   desc "setvars"
   task :setvars do
     set_vars
+  end
+
+  desc "setvars"
+  task :print_env do
+    set_vars
+    #EbFastDeploy::OPTIONS.each do |k,v|
+    #  puts "#{v[:namespace]} #{v[:option_name]}=#{ENV[k]}"
+    #end
+
+    puts "---------------  RDS AND NETWORK --------------\n"
+    rdb_network_options
+    rdb_network_options.each do |opt|
+      puts "#{opt[:namespace]} #{opt[:option_name]}=#{opt[:value]}"
+    end
+
+    puts "---------------  RUBY CONTAINER  --------------\n"
+    rails_options.each do |opt|
+      puts "#{opt[:namespace]} #{opt[:option_name]}=#{opt[:value]}"
+    end
+    puts all_options
   end
 
   desc "assets compile and upload to S3"
@@ -188,7 +305,7 @@ namespace :eb do
       AWS.elastic_beanstalk.client.create_environment(:application_name => ENV['APP_NAME'],
                                                   :environment_name => ENV['ENVIRONMENT'],
                                                   :solution_stack_name => ENV['STACK'],
-                                                  :option_settings => rails_options)
+                                                  :option_settings => all_options)
       puts "(Info) Environment \"#{ ENV['ENVIRONMENT'] }\" created"
     else
       puts "(Warning) Environment \"#{ ENV['ENVIRONMENT'] }\" already exists"
@@ -212,7 +329,7 @@ namespace :eb do
       options_to_remove.each{|opt| opt.delete(:value) }
 
       AWS.elastic_beanstalk.client.update_environment(:environment_name => ENV['ENVIRONMENT'],
-                                                 :option_settings => rails_options,
+                                                 :option_settings => all_options,
                                                  :options_to_remove => options_to_remove )
 
       new_env_config = AWS.elastic_beanstalk.client.describe_configuration_settings(:application_name=>ENV['APP_NAME'], :environment_name => ENV['ENVIRONMENT'])
